5e5055779203296efba4857a96c6a8830c331ae9
Andy Murin <murinandy@gmail.com>
2024-05-07 14:21:52 +0200

    my commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="sticky flex top-0 bg-gradient-to-r from-amber-400 to-amber-600">
+    <div class="flex items-center p-2 text-white">
+      <router-link to="/">
+        <div class="flex items-center p-2 gap-2">
+          <img src="./assets/logo.svg" alt="logo" width="100" />
+          <p class="text-2xl font-bold">Bratislavská Piváreň</p>
+        </div>
+      </router-link>
+
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+  <router-view />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
